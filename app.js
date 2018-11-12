8c37e693840927576748eae068d2c0643adf1c1f
AlexanderV <avaughan@barefootsolutions.com>
2018-11-12 15:14:31 -0800

    Fix IO not defined

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ io.on('connection', function(socket){
     });
 });
 
-// http.listen(3000, function(){
-//     console.log('listening on *:3000');
-// });
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
 
 module.exports = app;
