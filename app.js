59418f2281e0a9311e727cfea449dbe0c7abc226
AlexanderV <avaughan@barefootsolutions.com>
2018-11-21 15:09:04 -0800

    Pass in ID variable

--- app.js
+++ app.js
@@ -7,14 +7,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chatRouter = require('./routes/chat');
 
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-// app.get('/custom', function(req, res){
-//     res.sendFile(__dirname + '/views/index.ejs');
-// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
